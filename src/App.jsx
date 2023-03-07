744e9b65399f03ff1d63f125b2ff7bb22cf725d5
Marianne Agulhon <marianne.agulhon@gmail.com>
2023-03-07 17:23:45 +0100

    Do iteration 6

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Layout from "./component/Navbar/Navbar";
 import AllBeers from "./pages/AllBeers";
 import SingleBeer from "./pages/SingleBeer";
 import RandomBeer from "./pages/RandomBeer";
+import NewBeer from "./pages/NewBeer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/beers" element={<AllBeers />} />
           <Route path="/beers/:beerId" element={<SingleBeer />} />
           <Route path="/random-beer" element={<RandomBeer />} />
+          <Route path="/new-beer" element={<NewBeer />} />
         </Route>
       </Routes>
     </div>
